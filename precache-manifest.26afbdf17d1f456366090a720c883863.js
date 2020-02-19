2c674eeb18857e77dadb1112b65bb70eedecfcf9
LiorMBH <liorbh@gmail.com>
2020-02-19 13:08:21 +0200

    Updates

---
+++ precache-manifest.26afbdf17d1f456366090a720c883863.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08c4621f664c60019756bce285c94c2",
+    "url": "/LiorMBH/pixelPerfect/index.html"
+  },
+  {
+    "revision": "ee5b80d15512b1c4e336",
+    "url": "/LiorMBH/pixelPerfect/static/css/main.f4e798e1.chunk.css"
+  },
+  {
+    "revision": "691ab45c3fad875dcce7",
+    "url": "/LiorMBH/pixelPerfect/static/js/2.5627ec7d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/LiorMBH/pixelPerfect/static/js/2.5627ec7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5b80d15512b1c4e336",
+    "url": "/LiorMBH/pixelPerfect/static/js/main.5cd8e824.chunk.js"
+  },
+  {
+    "revision": "d8692785e2d4dad8a140",
+    "url": "/LiorMBH/pixelPerfect/static/js/runtime-main.18ba01b7.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/LiorMBH/pixelPerfect/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "686d9685b805c407876cd1abd698fd73",
+    "url": "/LiorMBH/pixelPerfect/static/media/Livvic-Regular.686d9685.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/LiorMBH/pixelPerfect/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/LiorMBH/pixelPerfect/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/LiorMBH/pixelPerfect/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/LiorMBH/pixelPerfect/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/LiorMBH/pixelPerfect/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "22c2459ee9b2f81c568b15fdfa3e84bf",
+    "url": "/LiorMBH/pixelPerfect/static/media/logo.22c2459e.svg"
+  }
+]);
